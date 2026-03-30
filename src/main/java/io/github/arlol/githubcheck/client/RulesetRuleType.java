19d0d9249fe8a16a8dc6ef3f4cd2c3e5c58003e2
Arlo O'Keeffe <tiger@k5d.de>
2026-03-30 21:40:00 +0200

    Add all GitHub ruleset rule types to RulesetRuleType enum

--- src/main/java/io/github/arlol/githubcheck/client/RulesetRuleType.java
+++ src/main/java/io/github/arlol/githubcheck/client/RulesetRuleType.java
@@ -10,5 +10,20 @@ public enum RulesetRuleType {
 	PULL_REQUEST, @JsonProperty("creation")
 	CREATION, @JsonProperty("update")
 	UPDATE, @JsonProperty("deletion")
-	DELETION
+	DELETION, @JsonProperty("required_signatures")
+	REQUIRED_SIGNATURES, @JsonProperty("commit_message_pattern")
+	COMMIT_MESSAGE_PATTERN, @JsonProperty("commit_author_email_pattern")
+	COMMIT_AUTHOR_EMAIL_PATTERN, @JsonProperty("committer_email_pattern")
+	COMMITTER_EMAIL_PATTERN, @JsonProperty("branch_name_pattern")
+	BRANCH_NAME_PATTERN, @JsonProperty("tag_name_pattern")
+	TAG_NAME_PATTERN, @JsonProperty("file_path_restriction")
+	FILE_PATH_RESTRICTION, @JsonProperty("max_file_path_length")
+	MAX_FILE_PATH_LENGTH, @JsonProperty("file_extension_restriction")
+	FILE_EXTENSION_RESTRICTION, @JsonProperty("max_file_size")
+	MAX_FILE_SIZE, @JsonProperty("workflows")
+	WORKFLOWS, @JsonProperty("code_scanning")
+	CODE_SCANNING, @JsonProperty("copilot_code_review")
+	COPILOT_CODE_REVIEW, @JsonProperty("required_deployments")
+	REQUIRED_DEPLOYMENTS, @JsonProperty("merge_queue")
+	MERGE_QUEUE
 }
